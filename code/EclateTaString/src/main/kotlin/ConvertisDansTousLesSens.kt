10913e6d755325347248e7424fe6aa434f65be90
Nathan D'Argenzio <2382694@cegepmontpetit.ca>
2024-08-29 12:51:48 -0400

    PROGRES exercice Convertis dans tous les sens

---
+++ code/EclateTaString/src/main/kotlin/ConvertisDansTousLesSens.kt
@@ -0,0 +1,16 @@
+package org.dargenzio
+
+fun main(){
+
+    //int to float
+    val nombreInt: Int = 23
+    val nombreFloat = nombreInt.toFloat()
+    println(nombreFloat)
+
+    //Double to int
+    val nombreDoub: Double = 42.21
+    val nombreInt2 = nombreDoub.toInt()
+    println(nombreInt2)
+
+    //String to int
+}
