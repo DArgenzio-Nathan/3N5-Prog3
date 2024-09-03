d12a24daea9084f5d2212ad5cc6dc040ece7951f
Nathan D'Argenzio <2382694@cegepmontpetit.ca>
2024-09-03 08:30:39 -0400

    FCT exercice convertis dans tous les sens

--- code/EclateTaString/src/main/kotlin/ConvertisDansTousLesSens.kt
+++ code/EclateTaString/src/main/kotlin/ConvertisDansTousLesSens.kt
@@ -13,4 +13,18 @@ fun main(){
     println(nombreInt2)
 
     //String to int
+    val unstring: String = "10"
+    val nombreInt3 = unstring.toInt()
+    println(nombreInt3)
+
+    //int to string
+    val nombreInt4: Int = 56
+    val unstring2 = nombreInt4.toString()
+    println(unstring2)
+
+    //float to double
+    val nombreFloat2: Float = 23.2F
+    val nombreDouble2 = nombreFloat2.toDouble()
+    println(nombreDouble2)
+
 }
